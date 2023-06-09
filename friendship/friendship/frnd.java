e175c025a3ad063e6715fab7c8a71e19f85a68f1
online lab <online lab@DESKTOP-NN6G1DN>
2023-06-08 22:42:33 -0700

    2nd commit

---
+++ friendship/friendship/frnd.java
@@ -0,0 +1,43 @@
+package friendship;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class frnd {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frnd window = new frnd();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public frnd() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
